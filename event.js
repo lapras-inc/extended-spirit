4fb9333eeef8e837f69ce9874891a0422e7ee88f
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-03-11 19:05:59 +0900

    now

--- event.js
+++ event.js
@@ -1,19 +1,26 @@
 chrome.runtime.onInstalled.addListener(() => {
-    const parent = chrome.contextMenus.create({
-      id: 'parent',
-      title: '親メニュー'
-    });
-    chrome.contextMenus.create({
-      id: 'child1',
-      parentId: 'parent',
-      title: '子メニュー1'
-    });
+  const parent = chrome.contextMenus.create({
+    id: 'parent',
+    title: '親メニュー'
   });
-  
-  // メニューをクリック時に実行
-  chrome.contextMenus.onClicked.addListener(item => {
-    console.log(item);
-    console.log(item.menuItemId);
-    alert(item);
+  chrome.contextMenus.create({
+    id: 'child1',
+    parentId: 'parent',
+    title: '子メニュー1'
   });
-  
+});
+
+// メニューをクリック時に実行
+chrome.contextMenus.onClicked.addListener(item => {
+  console.log(item);
+  console.log(item.menuItemId);
+
+
+  fetch('https://app.holaspirit.com/api/me', {
+    headers: {
+      'authorization': 'Bearer default:xxxx'
+    }
+  }).then(response => response.json()).then((resp) => {
+    alert(JSON.stringify(resp));
+  });
+});
