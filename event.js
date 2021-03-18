997eddeecb07e3e7749301035e84f05454652d49
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-03-18 19:33:25 +0900

    now

--- event.js
+++ event.js
@@ -16,24 +16,44 @@ chrome.contextMenus.onClicked.addListener(item => {
   console.log(item.menuItemId);
 
 
-  getProject('6048000e343b9430fb74ecf9', '6048054642d878641215a440')
+  // getProject('6048000e343b9430fb74ecf9', '6048054642d878641215a440')
+  createProject('6048000e343b9430fb74ecf9')
 
   // fetch('https://app.holaspirit.com/api/me', {
   //   headers: {
-  //     'authorization': 'Bearer default:NzM3NWQwYWE4M2U5MzZkMTUxNDRkOTQwZWRhZTFjMWJhZWIyNGM0NTQ4YTllZDUwZjRjY2FlNzY5MGE0OTg1Mg'
+  //     'authorization': 'Bearer default:xxx'
   //   }
   // }).then(response => response.json()).then((resp) => {
   //   alert(JSON.stringify(resp));
   // });
 });
 
+
+
+
 const getProject = async (organizationId, projectId) => {
   fetch(`https://app.holaspirit.com/api/organizations/${organizationId}/projects/${projectId}`, {
     headers: {
-      'authorization': 'Bearer default:NzM3NWQwYWE4M2U5MzZkMTUxNDRkOTQwZWRhZTFjMWJhZWIyNGM0NTQ4YTllZDUwZjRjY2FlNzY5MGE0OTg1Mg'
+      'authorization': 'Bearer default:xxx'
     }
   }).then(response => response.json()).then((resp) => {
     alert(JSON.stringify(resp));
   }).catch(error => {alert(error.message)});
 }
 
+const createProject = async (organizationId) => {
+  fetch(`https://app.holaspirit.com/api/organizations/${organizationId}/projects`, {
+    method: 'POST',
+    headers: {
+      'authorization': 'Bearer default:xxx',
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      title: '素晴らしいプロジェクト',
+      circle: '6048015b409fe014e330f40c',
+      status: 'current',
+    }),
+  }).then(response => response.json()).then((resp) => {
+    alert(JSON.stringify(resp));
+  }).catch(error => {alert(error.message)});
+};
